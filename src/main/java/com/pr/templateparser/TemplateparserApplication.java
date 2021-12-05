99d4a600d9b74dde92e859083719767735daf50b
Sum88t <krellexkrellex@gmail.com>
2021-12-05 18:57:04 +0530

    initial commit

---
+++ src/main/java/com/pr/templateparser/TemplateparserApplication.java
@@ -0,0 +1,13 @@
+package com.pr.templateparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateparserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateparserApplication.class, args);
+	}
+
+}
